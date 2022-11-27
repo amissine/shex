f212afbb57054f0ad8de6637bbfc820180ed26ae
Alec Missine <alec_missine@yahoo.com>
2022-11-27 15:48:57 -0500

    Bug fix

--- shex/index.js
+++ shex/index.js
@@ -21,7 +21,8 @@ async function setup (state, setState) { // {{{1
     }
     for (let asset of network.hex.assets) {
       await user.trust(asset).sign(window.freighterApi)
-        .then(xdr => user.submit({ xdr })).catch(e => console.error(e))
+        .then(xdr => user.submit({ xdr }))
+        .catch(e => console.error('UNEXPECTED', e))
     }
     console.log('user', user)
     setState(p => Object.assign({}, p, { user, }))
