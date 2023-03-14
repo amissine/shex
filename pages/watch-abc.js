abb55e58bf5bf9eaa920440fa28281927d89db47
Alec Missine <alec_missine@yahoo.com>
2023-03-14 07:52:23 -0400

    Update watch-abc

--- pages/watch-abc.js
+++ pages/watch-abc.js
@@ -8,7 +8,7 @@ import { FAPI_READY, NO_WALLET, SDK_READY, flag, setupNetwork, } from '../shex'
 import { Semaphore, retrieveItem, storeItem, timestamp, } from '../foss/utils.mjs'
 import { Account, } from '../foss/stellar-account.mjs'
 
-let timeoutMs = 9000, lock = new Semaphore(1) // {{{1
+let timeoutMs = 4000, count = 0 //, lock = new Semaphore(1) // {{{1
 
 function delta (label) { // {{{1
   let ts = timestamp(label)
@@ -19,28 +19,21 @@ function effect4agent (e, set) { // {{{1
   if (e.type != 'account_debited' || e.asset_code != 'HEXA') {
     return;
   }
-  lock.acquire().then(_ => e.operation()).then(o => e4u(set, e, o))
+  e.operation().then(o => e4u(set, e, o))
 }
 
 function e4u (set, e, o) { // {{{1
-  let item = {}
-  let server = window.StellarHorizonServer
-  let keypair = window.StellarSdk.Keypair.fromPublicKey(o.to)
-  new Account({ keypair }).load().then(u => server.effects().forAccount(u.loaded.id).stream({
+  let itemRemoved = false
+  let close = window.StellarHorizonServer.effects().forAccount(o.to).stream({
     onerror:   r => console.error(r),
-    onmessage: m => userInfo(m, item)
-  })).then(close => setTimeout(_ => {
-    Object.assign(item, {
-      account_funded: o.to,
-      amount_funded: e.amount,
-      created_at: e.created_at,
-      id: e.id,
-    })
-    set(p => Object.assign({}, p, { posts: p.posts.concat([item]) }))
-    close()
-    lock.release()
-    console.log('- user stream closed, lock released')
-  }, 600))
+    onmessage: m => {
+      let item = { account_funded: o.to, amount_funded: e.amount, count: ++count }
+      userInfo(m, item) && set(p => Object.assign({}, p, { posts: p.posts.concat([item]) }))
+      item.removed && close()
+      itemRemoved = item.removed
+    }
+  })
+  setTimeout(_ => itemRemoved || close(), 900)
 }
 
 function userInfo (m, item) { // {{{1
@@ -48,15 +41,24 @@ function userInfo (m, item) { // {{{1
     item.started = true
     return;
   }
-  if (m.type != 'data_created') {
-    return;
+  item.created_at = m.created_at
+  switch (m.type) {
+    case 'data_created':
+      item.id = m.id
+      item[m.name] = Buffer.from(m.value, 'base64').toString()
+      return m.name == 'greeting';
+    //case 'trustline_removed':
+    case 'account_removed':
+      item.id = m.id
+      item.removed = true
+      return true;
   }
-  item[m.name] = Buffer.from(m.value, 'base64').toString()
-  console.log(delta(m.account), item)
 }
 
 export default function WatchAnnBenCyn() { // {{{1
   const [wABC, setQ] = useState({}) // {{{2
+  const bottomRef = useRef(null) // thanks to https://bobbyhadz.com/blog/react-scroll-to-bottom
+
   const streams = useRef([]), timeoutId = useRef({}), agent = useRef({}), close = useRef({})
   const timeoutFn = _ => {
     clearTimeout(timeoutId.current)
@@ -74,14 +76,13 @@ export default function WatchAnnBenCyn() { // {{{1
     let server = window.StellarHorizonServer
     let agentPK = window.StellarNetwork.hex.agent
     let keypair = window.StellarSdk.Keypair.fromPublicKey(agentPK)
-    agent.current = await new Account({ keypair }).load()
-    streams.current.push(server.effects().forAccount(agent.current.loaded.id).stream({
+    streams.current.push({ close: server.effects().forAccount(agentPK).stream({
       onerror:   e => console.error(e),
       onmessage: e => effect4agent(e, set) || resetTimeout()
-    }))
+    }) })
     close.current = _ => {
-      for (let close of streams.current) {
-        close()
+      for (let stream of streams.current) {
+        stream.close()
       }
       console.log('- teardown,', streams.current.length, 'streams closed')
     }
@@ -100,6 +101,7 @@ export default function WatchAnnBenCyn() { // {{{1
         wABC.network || flag(flags, NO_WALLET) && setQ(_ => ({ network }))
       default:
         network && !window.StellarNetwork && setupNetwork(network) && setup(setQ)
+        bottomRef.current?.scrollIntoView({behavior: 'smooth'})
     }
   }, [wABC])
   return ( // {{{2
@@ -137,6 +139,7 @@ and  <span className={abc.cyn}>Cyn</span>
 {` on Stellar ${wABC.network}`}
       </h1>
       <Posts list={wABC.posts} />
+      <div ref={bottomRef} />
     </div> {/* }}}3 */}
   </>
   ); // }}}2
@@ -146,12 +149,15 @@ function Posts ({ list }) { // {{{1
   if (!list?.length) {
     return;
   }
-  return list.map(item =>
+  return list.map(item => {
+    let pk = item.account_funded.slice(0, 4) + '...' + item.account_funded.slice(-4)
+    let post = item.removed ? `${item.created_at}: account ${pk} removed`
+    : `${item.created_at}: account for user ${item.greeting} funded`
+    post += ', count ' + item.count
+    return (
     <Fragment key={item.id}>
-      <article className={abc.agent}>
-{item.created_at}: created and funded account {item.account_funded} 
-{' '} for user {item.greeting} with HEXA {item.amount_funded}
-      </article>
+      <article className={abc.agent} title={item.account_funded} >{post}</article>
     </Fragment>
-  );
+    );
+  });
 }
